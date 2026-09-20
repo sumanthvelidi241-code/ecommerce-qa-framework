77e65d64e5504dcb5821ee366e1e88a9cdacf917
VELIDI SUMANTH <sumsnthvelidi241@gmail.com>
2026-09-20 11:42:23 +0530

    feat: add TestBase webdriver setup and configure testng compile scope

---
+++ src/main/java/com/saucedemo/base/TestBase.java
@@ -0,0 +1,30 @@
+package com.saucedemo.base;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+    
+    protected static WebDriver driver;
+    protected final String BASE_URL = "https://www.saucedemo.com/";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
