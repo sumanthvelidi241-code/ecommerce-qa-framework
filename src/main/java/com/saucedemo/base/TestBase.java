c4c9bcd90ef099acb58a26cc8a12655d2978ec90
VELIDI SUMANTH <sumsnthvelidi241@gmail.com>
2026-09-20 12:08:01 +0530

    fix: configure ChromeOptions in TestBase to suppress password leak popup

--- src/main/java/com/saucedemo/base/TestBase.java
+++ src/main/java/com/saucedemo/base/TestBase.java
@@ -1,8 +1,11 @@
 package com.saucedemo.base;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,7 +18,20 @@ public class TestBase {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        // Configure Chrome preferences to suppress password alerts & save prompts
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(BASE_URL);
